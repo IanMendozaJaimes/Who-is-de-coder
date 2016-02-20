356f0fd00692ff3834b3c549c697b33850c7ebd7
Ian Mendoza Jaimes <ianmj2013@gmail.com>
2016-02-20 14:15:35 -0600

    modelos

--- hackaton/models.py
+++ hackaton/models.py
@@ -1,5 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from users.models import Coder
 
 
-# Create your models here.
-class hackaton(models.Model):
+class Equipos(model.Models):
+    nombreEquipo = models.CharField(max_length=255)
+    nombreProyecto = models.CharField(max_length=255)
+    descripcionProyecto = models.TextField(default="None")
+    participantes = models.ManyToManyField(Coder)
+
+
+class Hackaton(models.Model):
+    nombreHackaton = models.CharField(max_length=255)
+    lugar = models.CharField(max_length=255)
+    descripcion = models.TextField(default="None")
+    fecha = models.DateTimeField(auto_now=False)
+    equipos = models.ManyToManyField(Equipos)
--- users/models.py
+++ users/models.py
@@ -1,10 +1,47 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-class Reclutador(models.Model):
-    empresa = models.CharField(max_length=255)
-    lugarEmpresa = models.CharField(max_length=255)
+from hackaton.models import Hackaton
 
 
 class Lenguaje(models.Model):
     nombreLenguaje = models.CharField(max_length=255)
+    indefinido = 'indefinido'
+
+    objetivoTupla = (
+        (indefinido, 'indefinido')
+        ('objetos', 'orientado a objetos'),
+        ('eventos', 'orientado a eventos'),
+        ('hibrido', 'hibrido'),
+    )
+    objetivo = models.CharField(max_length=255, choices=objetivoTupla, default=indefinido)
+
+
+
+class Reclutador(models.Model):
+    empresa = models.CharField(max_length=255)
+    lugarEmpresa = models.CharField(max_length=255)
+    lenguajes = models.ManyToManyField(Lenguaje)
+    usuario = models.OneToOneField(User)
+
+
+class Organizador(models.Models):
+    hackatones = ManyToManyField(Hackaton)
+    usuario = models.OneToOneField(User)
+
+
+class Coder(models.Model):
+    github = models.CharField(max_length=255)
+    linkedin = models.CharField(max_length=255)
+    nickname = models.CharField(max_length=255)
+    lugarVive = models.CharField(max_length=255)
+
+    remoto = 'remoto'
+    presencial = 'presencial'
+    dispone = (
+        (remoto, 'remoto'),
+        (presencial, 'presencial')
+    )
+
+    disponibilidad = models.CharField(max_length=255, choices=dispone, default=presencial)
+
+    usuario = models.OneToOneField(User)
