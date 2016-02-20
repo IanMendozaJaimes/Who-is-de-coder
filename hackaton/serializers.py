f63d5ec419c993e8594689a0a7632a031186f33c
Juan Daniel Castillo Reyes <castilloreyesjuan@gmail.com>
2016-02-20 16:57:03 -0600

    API

--- hackaton/serializers.py
+++ hackaton/serializers.py
@@ -1,9 +1,22 @@
 from .models import Hackaton
 from rest_framework import serializers
+from django.utils.timezone import now
 
 
 class HackatonSerializer(serializers.HyperlinkedModelSerializer):
+    paso = serializers.SerializerMethodField()
+
+    def get_paso(self, obj):
+        obj.si = (obj.fecha - now()).days
+        print ("Hola %d" % obj.si)
+
+        if obj.si <= 0:
+            obj.paso = 1
+        else:
+            obj.paso = 0
+
+        return obj.paso
 
     class Meta:
         model = Hackaton
-        fields = ('id', 'nombreHackaton',)
+        fields = ('id', 'nombreHackaton', 'lugar', 'fecha', 'paso')
