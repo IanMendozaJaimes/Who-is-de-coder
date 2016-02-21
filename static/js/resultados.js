f148fe107f0081281c3df256ef5b5bac25492aa7
EliothMonroy <eliothmonroy@yahoo.com.mx>
2016-02-21 10:08:11 -0600

    busqueda tecnologias

--- static/js/resultados.js
+++ static/js/resultados.js
@@ -1,7 +1,7 @@
 (function(){
     var id_reclutador;
     id_reclutador=$('.nickname_val').val();
-
+    var keyword=$(.keyword).val();
 	var obtener_resultados = function(){
 		//GET ultimos hackatones
 		var container = $(".resultados-busqueda");
@@ -9,16 +9,16 @@
             template = "<div class='resultados'>"+
                 +"<h3 class='nombre-proyecto'>:nombre-proyecto:</h3>"+
                 "<ul class='resultado-data'>"+
+                    "<li class='nombre-equipo'>:nombre-equipo:</li>"+
                     "<li class='language'>:language:</li>"+
-                    "<li class='fecha'>:fecha:</li>"+
                     "<li class='id'>:id:</li>"
                 "</ul>"+
             "</div>";
-		$.get("/find",function(data){
+		$.get("/find/"+keyword,function(data){
 			data.forEach(function(item){
-				var binding = template.replace(":nombre-proyecto:", item.nombre)
-				.replace(":language:", item.language)
-				.replace(":fecha:", item.fecha_format)
+				var binding = template.replace(":nombre-proyecto:", item.nombreProyecto)
+                .replace(":nombre-equipo:", item.nombreEquipo)
+				.replace(":language:", item.tecnologias)
 				.replace(":id:", item.id)
 				container.append($(binding).fadeIn(1500));
 			});
