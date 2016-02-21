8215ef839c267d999c7913bf19e512a4a1fd9069
Carlos Tonatihu <carlostonatihu@gmail.com>
2016-02-21 01:07:21 -0600

    Nada

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 #from hackaton.models import Hackaton
-
+# sury <3
 
 class Lenguaje(models.Model):
     nombreLenguaje = models.CharField(max_length=255)
